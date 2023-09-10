3a996aac786c04cecf7765c63d26ffe66a08750f
Alfredo Pineda Prado <2100602f@umich.mx>
2023-09-10 02:54:31 -0600

    Primeros programas

--- main.java
+++ main.java
@@ -1,5 +1,13 @@
-public class main {
-    public static void main(String[] args){
-        System.out.println("probando git");
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Prueba class Ubicacion");
+        Ubicacion morelia = new Ubicacion(19.7007800, -101.1844300);
+        Ubicacion monterrey = new Ubicacion(25.67507, -100.31847);
+
+        double distancia_km = (morelia.distancia(monterrey) * 111.319);
+        System.out.println(distancia_km + " km");
+
     }
 }
