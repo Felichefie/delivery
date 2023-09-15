d0d18ec0373dc827f2df45c03b23d8878a8880e2
Elena <2134664d@umich.mx>
2023-09-15 08:58:56 -0600

    Modificaciones front

---
+++ Mimain.java
@@ -0,0 +1,7 @@
+public class Mimain{
+public static void main(String[] args) { // aqui tenemos el main, factible para hacer pruebas 
+
+        User user = new User();
+        user.show();
+    }
+}
--- User.java
+++ User.java
@@ -1,31 +1,30 @@
 import javax.swing.*;
 
 public class User {
-    private JLabel label;
-    private JButton button;
-    private JFrame frame;
+    private JLabel lbl_name, lbl_first, lbl_last, e_mail;
+    private JButton btn_cancel, btn_register;
+    private JFrame frm_user;
 
-    User() {
-        frame = new JFrame();
-        button = new JButton("Test");
-        button.setBounds(10, 50, 150, 100);
-        label = new JLabel("test");
-        label.setBounds(10,20,300,30);
+    User() { // es el constructor
+        frm_user = new JFrame();
+        btn_cancel = new JButton("Cancelar");
+        btn_cancel.setBounds(10, 50, 150, 100);
 
-        frame.add(label);
-        frame.add(button);
-        frame.setSize(400, 500);
-        frame.setLayout(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        btn_register = new JButton("Registrar");
+        btn_register.setBounds(10, 50, 150, 100);
+
+        lbl_name = new JLabel("Nombre");
+        lbl_name.setBounds(10,20,300,30);
+
+        frm_user.add(lbl_name);
+         frm_user.add(btn_cancel);
+         frm_user.setSize(400, 500);
+         frm_user.setLayout(null);
+         frm_user.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     } 
 
-    void show() {
-        frame.setVisible(true);
+    void show() { //Metodo
+        frm_user.setVisible(true);
     }
 
-
-    public static void main(String[] args) {
-        User user = new User();
-        user.show();
-    }
 }
