75313508d3e4dc8ed38ede31ce5d3c03e803a97f
JuanBelmontes <SkettK@gmail.com>
2023-09-15 08:52:53 -0600

    gui juan

--- User.java
+++ User.java
@@ -1,31 +1,33 @@
 import javax.swing.*;
 
 public class User {
-    private JLabel label;
-    private JButton button;
-    private JFrame frame;
+    private JLabel lbl_name, lbl_first, lbl_last, e_mail;
+    private JButton btn_cancel, btn_register;
+    private JFrame frm_user;
 
     User() {
-        frame = new JFrame();
-        button = new JButton("Test");
-        button.setBounds(10, 50, 150, 100);
-        label = new JLabel("test");
-        label.setBounds(10,20,300,30);
+        frm_user = new JFrame();
+        btn_cancel = new JButton("Test");
+        btn_cancel.setBounds(10, 50, 150, 100);
+        btn_register = new JButton("Registrar");
+        btn_register.setBounds(0, 0, 0, 0);
 
-        frame.add(label);
-        frame.add(button);
-        frame.setSize(400, 500);
-        frame.setLayout(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    } 
+        lbl_name = new JLabel("Nombre:");
+        lbl_name.setBounds(10, 20, 300, 30);
 
-    void show() {
-        frame.setVisible(true);
+        frm_user.add(lbl_name);
+        frm_user.add(btn_cancel);
+        frm_user.setSize(400, 500);
+        frm_user.setLayout(null);
+        frm_user.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    void mostrar() {
+        frm_user.setVisible(true);
+    }
 
     public static void main(String[] args) {
         User user = new User();
-        user.show();
+        user.mostrar();
     }
 }
