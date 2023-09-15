7f5ec9915d5fbdbd99941102b23153382003473a
Daniel <1912555e@umich.mx>
2023-09-15 07:10:00 -0600

    Mis Proyectos de la semana

---
+++ Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+    private Ubicacion ubicacion;
+    private Direccion direccion;
+    private String email;
+    private String nombre;
+    private String primer_apellido;
+    private String segundo_apellido;
+    private String telefono;
+
+    Usuario(Ubicacion ubicacion, Direccion direccion){
+        this.ubicacion = ubicacion;
+        this.direccion = direccion;
+
+    }
+    void imprimir_dirrecion(){
+        direccion.imprimir();
+
+    }
+    void imprimir_ubicacion(){
+        ubicacion.imprimir();
+
+    }
+    
+
+    
+}
