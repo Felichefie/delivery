21a2f00a5768a096eb13638a0e106df41418d513
ErickJMendoza <1628670a@umich.mx>
2023-09-15 09:00:54 -0600

    gui Erick

--- User.java
+++ User.java
@@ -1,31 +1,33 @@
 import javax.swing.*;
 
 public class User {
-    private JLabel label;
-    private JButton button;
-    private JFrame frame;
+    private JLabel l_nombre, l_materno, l_paterno;
+    private JButton btn_cancel, btn_register;
+    private JFrame frm_user;
 
     User() {
-        frame = new JFrame();
-        button = new JButton("Test");
-        button.setBounds(10, 50, 150, 100);
-        label = new JLabel("test");
-        label.setBounds(10,20,300,30);
+        frm_user = new JFrame();
+        btn_cancel = new JButton("Cancel");
+        btn_cancel.setBounds(10, 50, 150, 100);
+        
+        btn_register = new JButton("Registrar");
+        btn_register.setBounds(0, 0, 0, 0);
 
-        frame.add(label);
-        frame.add(button);
-        frame.setSize(400, 500);
-        frame.setLayout(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        l_nombre = new JLabel("Nombre");
+        l_nombre.setBounds(10,20,300,30);
+
+        frm_user.add(l_nombre);
+        frm_user.add(btn_cancel);
+        frm_user.setSize(400, 500);
+        frm_user.setLayout(null);
+        frm_user.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     } 
 
     void show() {
-        frame.setVisible(true);
+        frm_user.setVisible(true);
     }
 
 
-    public static void main(String[] args) {
-        User user = new User();
-        user.show();
-    }
+    
 }
