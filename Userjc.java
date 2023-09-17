072490736c2f6fdadb3c2599608d60622dfe9763
Jaime Jair Raya Castellon <2001347h@umich.mx>
2023-09-17 11:49:23 -0600

    agregué la parte de las etiquetas y textos

--- Userjc.java
+++ Userjc.java
@@ -37,10 +37,61 @@ public class Userjc {
         labelu.setBounds(229, 5, 250, 25);
         frame.add(labelu);
 
+        labeld = new JLabel("DATOS DEL USUARIO:");
+        labeld.setBounds(145, 35, 250, 25);
+        frame.add(labeld);
 
+        labelt = new JLabel("Nombre completo:");
+        labelt.setBounds(5, 75, 250, 25);
+        frame.add(labelt);
+        textu = new JTextField(30);
+        textu.setBounds(5, 100, 320, 15);
+        frame.add(textu);
+
+        labelc = new JLabel("Primer apellido:");
+        labelc.setBounds(5, 130, 250, 25);
+        frame.add(labelc);
+        textd = new JTextField(20);
+        textd.setBounds(5, 150, 320, 15);
+        frame.add(textd);
+
+        labelc = new JLabel("Segundo apellido:");
+        labelc.setBounds(5, 180, 250, 25);
+        frame.add(labelc);
+        textt = new JTextField(30);
+        textt.setBounds(5, 200, 320, 15);
+        frame.add(textt);
+
+        labelci = new JLabel("Numero telefonico:");
+        labelci.setBounds(5, 230, 250, 25);
+        frame.add(labelci);
+        textc= new JTextField(12);
+        textc.setBounds(5, 250, 320, 15);
+        frame.add(textc);
+
+        labels = new JLabel("Email:");
+        labels.setBounds(5, 280, 250, 25);
+        frame.add(labels);
+        textc = new JTextField(30);
+        textc.setBounds(5, 300, 320, 15);
+        frame.add(textc);
+
+        labelsi = new JLabel("Dirección:");
+        labelsi.setBounds(5, 330, 250, 25);
+        frame.add(labelsi);
+        textci = new JTextField(80);
+        textci.setBounds(5, 350, 320, 15);
+        frame.add(textci);
+
+        labelo = new JLabel("Ubicación:");
+        labelo.setBounds(5, 380, 250, 25);
+        frame.add(labelo);
+        texts = new JTextField(60);
+        texts.setBounds(5, 400, 320, 15);
+        frame.add(texts);
 
         button = new JButton("Test");
-        button.setBounds(10, 50, 150, 100);
+        button.setBounds(500, 1000, 150, 100);
 
         frame.add(button);
         frame.setSize(400, 500);
