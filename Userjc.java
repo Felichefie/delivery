ccdb31bf0162d91734e77b01247a48fdcdff48b5
Jaime Jair Raya Castellon <2001347h@umich.mx>
2023-09-17 11:11:47 -0600

    mas avance

--- Userjc.java
+++ Userjc.java
@@ -17,15 +17,30 @@ public class Userjc {
     private JButton button;
     private JButton buttonu;
 
+//codigo para los textos que necesitamos ingresar.
+    private JTextField textu;
+    private JTextField textd;
+    private JTextField textt;
+    private JTextField textc;
+    private JTextField textci;
+    private JTextField texts;
+    private JTextField textsi;
+
+//codigo necesario para que nos aparezca la ventana del form
     private JFrame frame;
 
     Userjc() {
         frame = new JFrame();
+
+//damos nombre y dimensiones a las etiquetas que apareceran.
+        labelu = new JLabel("Jaime Jaír Raya Castellón");
+        labelu.setBounds(229, 5, 250, 25);
+        frame.add(labelu);
+
+
+
         button = new JButton("Test");
         button.setBounds(10, 50, 150, 100);
-        labelu = new JLabel("JAIRRR");
-        labelu.setBounds(10,20,300,30);
-        frame.add(labelu);
 
         frame.add(button);
         frame.setSize(400, 500);
