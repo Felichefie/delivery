05740008178e5a7ffda122e7dbe7f07f244e791b
Alfredo Pineda Prado <2100602f@umich.mx>
2023-09-18 07:37:43 -0600

    jaja

--- main.java
+++ main.java
@@ -1,13 +1,5 @@
-
-public class Main {
-
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Prueba class Ubicacion");
-        Ubicacion morelia = new Ubicacion(19.7007800, -101.1844300);
-        Ubicacion monterrey = new Ubicacion(25.67507, -100.31847);
-
-        double distancia_km = (morelia.distancia(monterrey) * 111.319);
-        System.out.println(distancia_km + " km");
-
+        println("probar git");
     }
 }
