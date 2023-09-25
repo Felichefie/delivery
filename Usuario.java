132f80ba417860e83e3c031ea391e625faf1e737
Elena <2134664d@umich.mx>
2023-09-25 08:44:53 -0600

    merge realizado

--- Usuario.java
+++ Usuario.java
@@ -12,9 +12,7 @@ public class Usuario {
         this.ubicacion = ubicacion;
         this.direccion = direccion;
     }
-    Usuario(Ubicacion ubicacion, Direccion direccion, String nombre, String primer_apellido, String segundo_apellido, String telefono, String email) {
-        this.ubicacion = ubicacion;
-        this.direccion = direccion;
+    Usuario( String nombre, String primer_apellido, String segundo_apellido, String telefono, String email) {
         this.email = email;
         this.nombre = nombre;
         this.primer_apellido = primer_apellido;
@@ -33,9 +31,12 @@ public class Usuario {
 
     void imprimir_datos_personales() {
         System.out.println(nombre);
-        System.out.println(email);
-        System.out.println(telefono);
-        System.out.println(segundo_apellido);
         System.out.println(primer_apellido);
+        System.out.println(segundo_apellido);
+        System.out.println(telefono);
+        System.out.println(email);
+       
+        
+       
     }
 }
