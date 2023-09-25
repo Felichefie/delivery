2f0e173090000bef0b02797f107ba043874be7be
Alexis <1701450H@umich.mx>
2023-09-25 08:00:24 -0600

    agregamos constructor a Usuario

--- Usuario.java
+++ Usuario.java
@@ -8,10 +8,20 @@ public class Usuario {
     private String telefono;
 
     
+    Usuario (Ubicacion ubicacion, Direccion direccion, String email, String nombre, String primer_apellido, String segundo_apellido, String telefono){
+        this.ubicacion = ubicacion;
+        this.direccion = direccion;
+        this.email = email;
+        this.nombre = nombre;
+        this.primer_apellido = primer_apellido;
+        this.segundo_apellido = segundo_apellido;
+        this.telefono = telefono;
+
+    }
+
     Usuario (Ubicacion ubicacion, Direccion direccion){
         this.ubicacion = ubicacion;
         this.direccion = direccion;
-        
     }
     void imprimir_direccion(){
         direccion.imprimir();
@@ -20,4 +30,14 @@ public class Usuario {
         void imprimir_ubicacion(){
         ubicacion.imprimir();
     }
+
+    void imprimir_datos_personales(){
+        System.out.println(this.nombre);
+        System.out.println(this.primer_apellido);
+        System.out.println(this.segundo_apellido);
+        System.out.println(this.email);
+        System.out.println(this.telefono);
+    }
+
+    
 }
