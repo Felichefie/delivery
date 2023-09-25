a172c3c56d9fc1a4fc84ee10649f2e1467124607
Alexis <1701450H@umich.mx>
2023-09-25 08:46:52 -0600

    union grafico con logico

--- ListernerButton.java
+++ ListernerButton.java
@@ -7,39 +7,48 @@ import java.awt.event.ActionListener;
 
 public class ListernerButton {
     JFrame f;
-    JTextField tf,tf2;
-    JButton btn,btn2;
+    JTextField tf_nombre,tf2_primer_apellido, tf3_segundo_apellido, tf4_email, tf5_telefono;
+    JButton btn_accept,btn_cancel;
 
     ListernerButton(){ // C O N S T R U C T O R
-        btn = new JButton("Press");
-        tf = new JTextField("Please press me");
+        btn_accept = new JButton("Aceptar");
+        tf_nombre = new JTextField("Alesos");
         f = new JFrame("Button Example");
-        btn2 = new JButton("Borrar");
+        btn_cancel = new JButton("Cancelar");
         f.setSize(600,300);
-        tf.setBounds(20,20,180,20);
-        tf2 = new JTextField("otro text field");
-        tf2.setBounds(20,40,180,20);        
-        btn.setBounds(160,160,80,20);
-        btn2.setBounds(180,180,80,20);
-        f.add(btn);
-        f.add(tf);
-        f.add(tf2);
-        f.add(btn2);
+        tf_nombre.setBounds(20,20,180,20);
+        tf2_primer_apellido = new JTextField("Coronao");
+        tf2_primer_apellido.setBounds(20,40,180,20);        
+        btn_accept.setBounds(160,160,120,20);
+        btn_cancel.setBounds(180,180,80,20);
+        tf3_segundo_apellido = new JTextField("Perez");
+        tf3_segundo_apellido.setBounds(20,60,180,20);
+        tf4_email = new JTextField("alesos@gmail.com");
+        tf4_email.setBounds(20,80,180,20);
+        tf5_telefono = new JTextField("+52 729 515 9174");
+        tf5_telefono.setBounds(20,100,180,20);
+        f.add(btn_accept);
+        f.add(tf_nombre);
+        f.add(tf2_primer_apellido);
+        f.add(btn_cancel);
+        f.add(tf3_segundo_apellido);
+        f.add(tf4_email);
+        f.add(tf5_telefono);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        btn.addActionListener(new ActionListener(){
+        btn_accept.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e){
-                System.out.println(tf2.getText());
-                System.out.println(tf.getText());
-
+                Usuario usuario = new Usuario(tf_nombre.getText(), tf2_primer_apellido.getText());
+                System.out.println(tf2_primer_apellido.getText());
+                System.out.println(tf_nombre.getText());
 
         }
     });
-    btn2.addActionListener(new ActionListener(){
+    btn_cancel.addActionListener(new ActionListener(){
         @Override
         public void actionPerformed(ActionEvent q){
-            tf.setText("");
-            tf2.setText("");
+            tf_nombre.setText("");
+            tf2_primer_apellido.setText("");
         }
     });
 
@@ -47,13 +56,11 @@ public class ListernerButton {
     public void mostrar(){
         f.setVisible(true);
     }
-
     public static void main(String[] args) { //M A I N
         ListernerButton lb;
         lb = new ListernerButton();
         lb.mostrar();
-        
-        System.out.println(lb.tf.getText());
+        System.out.println(lb.tf_nombre.getText());
         
     }
     
