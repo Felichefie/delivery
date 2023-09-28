dd23a8c9133a03d2da207b42e4869d19304d7f1b
Maria Itzel Hernandez Marin <121146186+itzelhde@users.noreply.github.com>
2023-09-27 22:04:50 -0600

    Update listenerbuttonitz.java

--- listenerbuttonitz.java
+++ listenerbuttonitz.java
@@ -2,7 +2,7 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.Action;
+//import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
