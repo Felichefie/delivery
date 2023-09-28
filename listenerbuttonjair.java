f5cc893842fe020eebaed749f3897a64277e704f
Jaime Jair Raya Castellon <143474279+JAIRCASTELLON@users.noreply.github.com>
2023-09-27 21:30:28 -0600

    ya merito acabamos si señor

--- listenerbuttonjair.java
+++ listenerbuttonjair.java
@@ -19,6 +19,18 @@ public class listenerbuttonjair {
         ft = new JTextField("push my");
         tf2 = new JTextField("otro gato");
 
+        f = new JFrame("BUTTON TEST");
+        f.setSize(600, 300);
+
+        tf.setBounds(20, 20, 180, 20);
+        tf2.setBounds(20, 40, 180, 20);
+        btn.setBounds(160, 160, 80, 20);
+        btnborrar.setBounds(180, 180, 80, 20);
+        f.add(btn);
+        f.add(btnborrar);
+        f.add(tf);
+        f.add(tf2);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
     }
     
