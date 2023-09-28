a6f458dce3a74acf2f14a40429423a8622ee2efc
Jaime Jair Raya Castellon <143474279+JAIRCASTELLON@users.noreply.github.com>
2023-09-27 21:19:42 -0600

    vamos bien rikillo

--- listenerbuttonjair.java
+++ listenerbuttonjair.java
@@ -1,6 +1,25 @@
 
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Action;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 
 public class listenerbuttonjair {
+
+    JFrame f;
+    JTextField ft, tf2;
+    JButton btn, btnborrar;
+    listenerbuttonjair () {
+        btn = new JButton("puchurrar");
+        btnborrar = new JButton("borrar:)");
+        ft = new JTextField("push my");
+        tf2 = new JTextField("otro gato");
+
+        
+    }
     
 }
