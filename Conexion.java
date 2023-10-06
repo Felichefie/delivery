d82ff91be98b59eccc9fb843e7642b17bbb707ae
Maria Itzel Hernandez Marin <121146186+itzelhde@users.noreply.github.com>
2023-10-05 21:53:27 -0600

    first version http conexion itzel

--- Conexion.java
+++ Conexion.java
@@ -1,3 +1,51 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.Identity;
+
+import javax.imageio.stream.ImageInputStream;
+
 public class Conexion {
+
+    private static final String URL_GET = "https://jsonplaceholder.typicode.com/";
+    private static final String METHOD_HTTP_GET = "GET";
+    HttpURLConnection connection;
+
+//CREAMOS EL CONSTRUCTOR.
+    Conexion(){
+        try{
+            URL object_url = new URL(URL_GET);
+            connection = (HttpURLConnection) object_url.openConnection();
+            connection.setRequestMethod(METHOD_HTTP_GET);
+
+
+        } catch (MalformedURLException e) {
+            //TODO AUTO-GENERATED CATCH BLOCK.
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     
+void sendGet() {
+    int response;
+    try {
+        response = connection.getResponseCode();
+
+        System.out.println(response);
+        BufferedReader br = new BufferedReader( new InputStreamReader(connection.getInputStream())); 
+        String line;
+
+        while(br.readLine() != null){
+            line = br.readLine();
+            System.out.println(line);
+        }
+    }catch(IOException e){
+        e.printStackTrace();
+    }
+}
 }
---
+++ ConnectionMain.java
@@ -0,0 +1,8 @@
+public class ConnectionMain {
+
+    public static void main(String[] args){
+        Conexion con = new Conexion();
+        con.sendGet();
+    }
+    
+}
