0fe8fc53270e6f2b8449bd58861a550532635b31
Daniel <1912555e@umich.mx>
2023-10-06 09:04:11 -0600

    Esta en obra negra aun

--- ConnectionMain.java
+++ ConnectionMain.java
@@ -1,6 +1,16 @@
+import java.util.List;
+
+import com.google.gson.Gson;
+
 public class ConnectionMain {
     public static void main(String[] args){
         Conexion con = new Conexion();
-        con.sendGet();
+        String txtJson = con.sendGet();
+        System.out.println(txtJson);
+
+        Gson gson = new Gson();
+
+        List<UserExample> objectUserExample = gson.fromJson(txtJson, List.class);
+        System.out.println(objectUserExample.size());
     }
 }
---
+++ UserExample.java
@@ -0,0 +1,39 @@
+public class UserExample {
+    private int userId;
+    private int id;
+    private String title;
+    private String body;
+    
+    public int getUserId(){
+        return this.userId;
+
+    }
+     public int getId(){
+        return this.id;
+     
+    }
+     public String getTitle(){
+        return this.title;
+        
+    }
+     public String getBody(){
+        return this.body;
+        
+    }
+
+    public void setUserId(int userId){
+        this.userId = userId;
+    }
+     public void setId(int Id){
+        this.id = Id;
+    }
+     public void setTitle(String title){
+        this.title = title;
+    }
+     public void setBody(String Body){
+        this.body = Body;
+
+
+
+    }
+}
