98f794aefef9d8a3e8fa07c8293987966b0cb4a6
FranciscoRamirez <1567126x@umich.mx>
2023-10-06 09:02:07 -0600

    No jalo 6 oct

--- ConectionMain.java
+++ ConectionMain.java
@@ -1,8 +1,19 @@
+import java.util.List;
+
+import com.google.gson.Gson;
+
 public class ConectionMain {
 
     public static void main(String[] args) {
         Conexion con = new Conexion();
-        con.sendGet();
+        String txtJson = con.sendGet();
+        System.out.println(txtJson);
+
+        Gson gson = new Gson();
+
+        List<UserExample> objectsUserExamples = gson.fromJson(txtJson, List.class);
+
+        //System.out.println(objectsUserExamples.size());
     }
     
 }
