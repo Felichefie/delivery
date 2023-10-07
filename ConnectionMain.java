a9dcf6f03d553b056345df7141ba21be49534df9
Jaime Jair Raya Castellon <143474279+JAIRCASTELLON@users.noreply.github.com>
2023-10-06 21:59:29 -0600

    first version http conexion jair

---
+++ ConnectionMain.java
@@ -0,0 +1,17 @@
+import  java.util.List;
+
+import com.google.gson.Gson;
+
+public class ConnectionMain {
+    public static void main(String[] args){
+        Conexion con = new Conexion();
+        String txtJson=con.sendGet();
+        System .out.println(txtJson);
+        Gson gson = new Gson();
+        List<Userexample> lista= gson.fromJson(txtJson, List.class);
+        System.out.println(lista.size());
+    }
+
+    
+
+}
