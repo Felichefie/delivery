acd7de013572505ddedd729d7f9a12dca65db9f5
JuanBelmontes <SkettK@gmail.com>
2023-10-09 07:51:19 -0600

    Se lo robé al Isra jasjadja

--- Conexion.java
+++ Conexion.java
@@ -1,22 +1,20 @@
-import java.net.URL;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.net.URL;
 
 public class Conexion {
-    private static final String URL_GET = "https://jsonplaceholder.typicode.com/posts/1";
+    private static final String URL_GET = "https://jsonplaceholder.typicode.com/posts";
     private static final String METHOD_HTTP_GET = "GET";
     HttpURLConnection connection;
 
     Conexion() {
         try {
-
             URL object_url = new URL(URL_GET);
             connection = (HttpURLConnection) object_url.openConnection();
             connection.setRequestMethod(METHOD_HTTP_GET);
-
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -25,21 +23,19 @@ public class Conexion {
     }
 
     String sendGet() {
-        String res = "";
         int response;
+        String res = "";
         try {
             response = connection.getResponseCode();
-            System.out.println(response);
-            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            // System.out.println(response);
+
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
             String line;
-
-            do {
-                line = br.toString();
-                // System.out.println(line); Yano sirve porque ya se guardo en res
-                // res = res + line; Significa lo mismo que lo de arriba
+            while ((line = br.readLine()) != null) {
                 res += line;
-            } while (br.readLine() != null);
-
+            }
+            // System.out.println(res);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- ConnectionMain.java
+++ ConnectionMain.java
@@ -1,11 +1,12 @@
 import java.util.List;
+
 import com.google.gson.Gson;
 
 public class ConnectionMain {
     public static void main(String[] args) {
         Conexion con = new Conexion();
         String txtJson = con.sendGet();
-        System.out.println(txtJson);
+        // System.out.println(txtJson);
         Gson gson = new Gson();
         List<UserExample> lista = gson.fromJson(txtJson, List.class);
         System.out.println(lista.size());
