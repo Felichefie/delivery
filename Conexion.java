449b4cd6ce166f6d888ea237219c58fdc04d7be8
Elena <2134664d@umich.mx>
2023-10-09 07:55:25 -0600

    listo

--- Conexion.java
+++ Conexion.java
@@ -6,16 +6,15 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Conexion {
-    private static final String URL_GET = "https://jsonplaceholder.typicode.com/posts/1";
-    private static final String MOTHOD_HTTP_GET = "GET";
+    private static final String URL_GET = "https://jsonplaceholder.typicode.com/posts";
+    private static final String METHOD_HTTP_GET = "GET";
     HttpURLConnection connection;
 
     Conexion() {
         try {
-            URL obj_url = new URL(URL_GET);
-            connection = (HttpURLConnection) obj_url.openConnection();
-            connection.setRequestMethod(MOTHOD_HTTP_GET);
-
+            URL object_url = new URL(URL_GET);
+            connection = (HttpURLConnection) object_url.openConnection();
+            connection.setRequestMethod(METHOD_HTTP_GET);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -24,26 +23,20 @@ public class Conexion {
     }
 
     String sendGet() {
-        int response;
         String res = "";
         try {
-            response = connection.getResponseCode();
+            int response = connection.getResponseCode();
             System.out.println(response);
-            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
             String line;
-            do {
-                line = br.readLine();
-                res = res + line;
-               // res += line;
-                //System.out.println(line);
+            while ((line = br.readLine()) != null) {
+                res += line;
             }
-            while (br.readLine() != null);
-            res = res + line;
-           
         } catch (IOException e) {
             e.printStackTrace();
         }
         return res;
     }
-
 }
