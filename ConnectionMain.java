1a00f623239476431cd1cc326c346cb272211b5f
Israel <1917663k@umich.mx>
2023-10-09 07:48:05 -0600

    ya jalo

--- ConnectionMain.java
+++ ConnectionMain.java
@@ -4,10 +4,10 @@ public class ConnectionMain {
     public static void main(String[] args) {
         Conexion conexion = new Conexion();
        String txtJson=conexion.sendGet();
-        System.out.println(txtJson);
+        //System.out.println(txtJson);// para ver el los datos
 
         Gson gson = new Gson();
         List<Userexample> lista = gson.fromJson(txtJson, List.class);
-        System.out.println(lista.size());
+        System.out.println("lista de registros: " + lista.size());// para ver el numero de datos que llegan
 }
 }
