81ff2400f638b74cdfcc4e529e94c7288b09c647
Alexis <1701450H@umich.mx>
2023-10-09 07:45:00 -0600

    Ya jalo tu

--- Conexion.java
+++ Conexion.java
@@ -1,44 +1,39 @@
-import java.net.URL;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.net.URL;
 
 public class Conexion {
-    private static final String URL_GET = "https://jsonplaceholder.typicode.com/posts/1";
+    private static final String URL_GET = "https://jsonplaceholder.typicode.com/posts";
     private static final String METHOD_HTTP_GET = "GET";
     HttpURLConnection connection;
-
-    Conexion() {
+    Conexion(){
         try {
-
             URL object_url = new URL(URL_GET);
             connection = (HttpURLConnection) object_url.openConnection();
             connection.setRequestMethod(METHOD_HTTP_GET);
-
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    String sendGet() {
-        String res = "";
+    String sendGet(){
         int response;
+        String res = "";
         try {
             response = connection.getResponseCode();
-            System.out.println(response);
-            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+           // sos System.out.println(response);
+
+            BufferedReader br = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
             String line;
-
-            do {
-                line = br.IoString();
+            while((line = br.readLine()) != null){
                 res += line;
-
-        
-            }  while (br.readLine() != null);
+            }
+            //System.out.println(res);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- ConnectionMain.java
+++ ConnectionMain.java
@@ -1,17 +1,14 @@
-
 import java.util.List;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+
 public class ConnectionMain {
     public static void main(String[] args) {
-    Conexion con = new Conexion();
-    String txtJson =  con.sendGet();
-        System.out.println(txtJson);
-    Gson gson = new Gson();
-List<UserExample> lista = gson.fromJson(txtJson, new TypeToken<List<UserExample>>(){}.getType());
-
+        Conexion con = new Conexion();
+        String txtJson=con.sendGet();
+        //System.out.println(txtJson);
+        Gson gson = new Gson();
+        List<UserExample> lista= gson.fromJson(txtJson, List.class);
         System.out.println(lista.size());
     }
-    }
-
+}
