b62d46b9a15f322d8136f3b800f5ab43782e5a44
Maria Itzel Hernandez Marin <121146186+itzelhde@users.noreply.github.com>
2023-10-10 21:07:03 -0600

    REGRESAR LOS OBJETOS DE LA PAGINA

--- ConnectionMain.java
+++ ConnectionMain.java
@@ -7,13 +7,21 @@ public class ConnectionMain {
     public static void main(String[] args){
         Conexion con = new Conexion();
         String txtJson = con.sendGet();
-        System.out.println(txtJson);
+        //System.out.println(txtJson);
 
         Gson gson = new Gson();
 
         List<Userexample> lista = gson.fromJson(txtJson, List.class);
 
         System.out.println("NUMERO DE REGISTROS" + lista.size());
+        int i;
+        for( i=0; i<lista.size(); i++){
+
+            //lista.get(i) regresa un objeto del tipo Userexample 
+                        //Userexample objecto = (Userexample) lista.get(i);
+            //System.out.println(lista.get(i).getUserId());
+
+        }
 
     }
     
