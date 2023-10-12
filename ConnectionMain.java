e001cf2afec1b89595ffeeb44182a8268d5b5ea4
Francisco Duran <85173479+FranciscoDuran17@users.noreply.github.com>
2023-10-11 20:48:19 -0600

    Add files via upload

--- ConnectionMain.java
+++ ConnectionMain.java
@@ -1,13 +1,21 @@
+import java.lang.reflect.Type;
 import java.util.List;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class ConnectionMain {
     public static void main(String[] args) {
         Conexion con = new Conexion();
         String txtJson=con.sendGet();
-        System.out.println(txtJson);
+        //System.out.println(txtJson);
         Gson gson = new Gson();
-        List<Userexample> lista= gson.fromJson(txtJson, List.class);
-        System.out.println(lista.size());
+        // List<Userexample> lista = gson.fromJson(txtJson, List.class);
+        Type type = new TypeToken<List<Userexample>>() {}.getType(); 
+        List<Userexample> lista = gson.fromJson(txtJson, type);
+        System.out.println("numero de registros: " + lista.size());
+
+        for(Userexample user : lista){
+            System.out.println(user.getUserId());
+        }        
     }
 }
