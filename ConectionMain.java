22c291db7f47d367e5df7791431146ea2a68b801
FranciscoRamirez <1567126x@umich.mx>
2023-10-12 23:33:54 -0600

    count ids diferentes y cada uno de ellos

--- ConectionMain.java
+++ ConectionMain.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -6,16 +7,42 @@ import com.google.gson.reflect.TypeToken;
 public class ConectionMain {
     public static void main(String[] args) {
         Conexion con = new Conexion();
-        String txtJson=con.sendGet();
-        //System.out.println(txtJson);
+        String txtJson = con.sendGet();
         Gson gson = new Gson();
-        // List<Userexample> lista = gson.fromJson(txtJson, List.class);
         Type type = new TypeToken<List<UserExample>>() {}.getType(); 
         List<UserExample> lista = gson.fromJson(txtJson, type);
-        System.out.println("numero de registros: " + lista.size());
+        System.out.println("Número de registros: " + lista.size());
 
-        for(UserExample user : lista){
-            System.out.println(user.getUserId());
-        }        
+        // Lista para almacenar los userId diferentes
+        List<Integer> diferentesUserIds = new ArrayList<>();
+
+        // Lista de listas para almacenar los id correspondientes a cada userId
+        List<List<Integer>> userIdsAndIds = new ArrayList<>();
+
+        for (UserExample user : lista) {
+            int userId = user.getUserId();
+            int id = user.getId();
+
+            // Si el userId no está en la lista distinctUserIds, agregarlo y crear una nueva lista para ese userId
+            if (!diferentesUserIds.contains(userId)) {
+                diferentesUserIds.add(userId);
+                List<Integer> idsForUser = new ArrayList<>();
+                idsForUser.add(id);
+                userIdsAndIds.add(idsForUser);
+            } else {
+                // Si el userId ya está en la lista distinctUserIds, encontrar la lista correspondiente y agregar el id a esa lista
+                int index = diferentesUserIds.indexOf(userId);
+                userIdsAndIds.get(index).add(id);
+            }
+        }
+
+        System.out.println("UserIds diferentes: " + diferentesUserIds);
+
+        // Imprimir los userId y sus id correspondientes
+        for (int i = 0; i < diferentesUserIds.size(); i++) {
+            int userId = diferentesUserIds.get(i);
+            List<Integer> ids = userIdsAndIds.get(i);
+            System.out.println("userId = " + userId + ", id" + ids);
+        }
     }
 }
