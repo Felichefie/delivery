6fe129c99fc27a5637cb2701b7bf0b18796bd361
Francisco Duran <85173479+FranciscoDuran17@users.noreply.github.com>
2023-10-13 20:37:44 -0600

    Manejo de objetos

--- ConnectionMain.java
+++ ConnectionMain.java
@@ -1,4 +1,6 @@
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -12,10 +14,33 @@ public class ConnectionMain {
         // List<Userexample> lista = gson.fromJson(txtJson, List.class);
         Type type = new TypeToken<List<Userexample>>() {}.getType(); 
         List<Userexample> lista = gson.fromJson(txtJson, type);
-        System.out.println("numero de registros: " + lista.size());
+        //System.out.println("numero de registros: " + lista.size());
 
+        List<Integer> userIds = new ArrayList<>();
+        List<List<Integer>> idLists = new ArrayList<>();
+        
         for(Userexample user : lista){
-            System.out.println(user.getUserId());
-        }        
+            int userId = user.getUserId();
+            int id = user.getId();
+            // Revisa si el userId ya existe en la lista
+            int index = userIds.indexOf(userId);
+            // Si no existe, agrega el userId a la lista de userIds y crea una nueva lista de ids
+            if(index == -1){
+                userIds.add(userId);
+                idLists.add(new ArrayList<Integer>());
+                // Actualiza el index del ultimo elemento
+                index = idLists.size() - 1;
+            }
+            // Si existe, obtiene la lista de userids y agrega el id
+            idLists.get(index).add(id);
+        }
+        
+        System.out.println("userId" + userIds.toString());
+        
+        for(int i = 0; i < userIds.size(); i++){
+            int userId = userIds.get(i);
+            List<Integer> ids = idLists.get(i);
+            System.out.println("userId =" + userId + ", id" + ids.toString());
+        }
     }
 }
