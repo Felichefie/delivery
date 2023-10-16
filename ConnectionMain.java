84f3ffab0ee328d08a54acfb47d1281041c5c66a
Jaime Jair Raya Castellon <143474279+JAIRCASTELLON@users.noreply.github.com>
2023-10-16 07:11:59 -0600

    entrega final

--- ConnectionMain.java
+++ ConnectionMain.java
@@ -1,5 +1,8 @@
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -13,13 +16,30 @@ public class ConnectionMain {
         Type type = new TypeToken<List<Userexample>>() {}.getType(); 
         List<Userexample> lista = gson.fromJson(txtJson, type);
         System.out.println("numero de registros: " + lista.size());
-            
 
-                        
-        
 
-            for(int userId >= 1; userId <= 10; userId++){
-                System.out.println("ID: " + id);
+        //------------------------------------------------------------------------
+        List<Integer> IdsUnicos = new ArrayList<Integer>();
+
+        for (Userexample uex : lista) {
+            if(!IdsUnicos.contains(uex.getUserId() ) )
+            IdsUnicos.add(uex.getUserId());
+        }
+        System.out.println("userId" + IdsUnicos.toString());
+
+        List<Integer> listado;
+
+        for(Integer id : IdsUnicos)
+        {
+            listado = new ArrayList<Integer>();
+            for(Userexample userX : lista)
+            {
+                if(id.equals(userX.getUserId()))
+                listado.add(userX.getId());
             }
+            System.out.println("listado para id: " + id);
+            System.out.println(listado);
+        }
+
         }        
     }
