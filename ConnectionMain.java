f1e26d16d0412674f14706469879a9a976447fff
SucioDan7u7 <143421314+SucioDan7u7@users.noreply.github.com>
2023-10-15 21:32:59 -0600

    :(

--- ConnectionMain.java
+++ ConnectionMain.java
@@ -1,5 +1,7 @@
+import java.lang.reflect.Type;
 import java.util.List;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class ConnectionMain {
     public static void main(String[] args) {
@@ -7,7 +9,16 @@ public class ConnectionMain {
         String txtJson=con.sendGet();
         System.out.println(txtJson);
         Gson gson = new Gson();
-        List<Userexample> lista= gson.fromJson(txtJson, List.class);
-        System.out.println(lista.size());
+        //List<Userexample> lista= gson.fromJson(txtJson, List.class);
+        Type type =new TypeToken<List<Userexample>>() {}.getType();
+        List<Userexample> lista = gson.fromJson(txtJson, type);
+        //System.out.println(lista.size());
+        System.out.println("numero de registro:" + lista.size());
+
+        for(Userexample user : lista) {
+            System.out.println ("USER ID: " + user.getUserId());
+            System.out.println ("ID: " + user.getUserId());
+
+        }
     }
 }
