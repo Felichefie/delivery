9c72dca6b8824eba1d040a22fbb95e347f4540dc
perlapaola22 <2001285B@Umich.mx>
2023-10-16 07:15:01 -0600

    Tarea

--- ConnectionMain.java
+++ ConnectionMain.java
@@ -1,55 +1,44 @@
-
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.google.gson.reflect.TypeToken;
+
 public class ConnectionMain {
-    int id;
-    public static <Userexample, UserrId> void main(String[] args){
-        Conexion  con = new Conexion();
+    public static void main(String[] args) {
+        Conexion con = new Conexion();
         String txtJson = con.sendGet();
-        //System.out.println(txtJson);
         Gson gson = new Gson();
-        
-        Type type = new TypeToken<List<Userexample>>() {}.getType(); 
+        Type type = new TypeToken<List<UserExample>>() {}.getType(); 
         List<UserExample> lista = gson.fromJson(txtJson, type);
-        System.out.println("numero de registros = " + lista.size());
-
-        //System.out.println(txtJson);  //IMPRIME TODOS LOS DATOS DE LA LISTA
-       // System.out.println("ID 1: " + lista.get(userId));
-        
-        /*for(int userId = 1; userId <= 10; userId++){
-            System.out.println("USER ID: " + userId);
-        }
-        Set<UserExample> conjunto = new HashSet<>(lista); //en conjunto para que no permita elementos duplicados
-        for(UserExample user: lista){
-            conjunto.add(user);                   
-        }*/
-        List<String> idsUnicos = new ArrayList<>();
-        for(UserExample user: lista){
-        if(!idsUnicos.contains(Integer.toString(user.getUserId()))){
-            idsUnicos.add(Integer.toString(user.getUserId()));
-
-        }
-        System.out.println("USERID" + idsUnicos.toString());
-        }
-       
-        
-         /*/
-        Integer todosIds [][];
-        todosIds = new Integer[[idsUnicos.size() + 1 ]][];
-        for(Userexample users: lista){
-            todosIds.getid();*/
-        }
-
+        System.out.println("Número de registros: " + lista.size());
 
         
+        List<Integer> UserIdUnicos = new ArrayList<>();
+        List<List<Integer>> userIdsAndIds = new ArrayList<>();
+
+        //recorre cada elemento de "Lista" que esta en User Example y asigna nombre a la variable user.
+        for (UserExample user : lista) {
+            int userId = user.getUserId();
+            int id = user.getId();
+
+            // Si el userId no está en la lista IdsUnicos, lo agrega.
+            if (!UserIdUnicos.contains(userId)) {
+                UserIdUnicos.add(userId);
+                List<Integer> idsFromUser = new ArrayList<>();
+                idsFromUser.add(id);
+                userIdsAndIds.add(idsFromUser);
+            } else {
+                // si el userId ya esta en la lista de IdsUnicos no lo agrega. agrega un Id a la lista de Id asociado a un UserId.
+                int index = UserIdUnicos.indexOf(userId);
+                userIdsAndIds.get(index).add(id);
+            }
+        }
+        System.out.println("UserIds diferentes: " + UserIdUnicos); //imprime los diferentes UserId que esta almacenado en la variable UserIdUnicos.
+        for (int i = 0; i < UserIdUnicos.size(); i++) {
+            int userId = UserIdUnicos.get(i);
+            List<Integer> ids = userIdsAndIds.get(i);
+            System.out.println("userId = " + userId + ", id" + ids);
+        }
     }
-    
-
-
+}
