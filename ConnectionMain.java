bac14f0e029c7248a202d1e36195abf9a606be1a
Israel <1917663k@umich.mx>
2023-10-15 22:05:45 -0600

    lo que no habia subido

--- ConnectionMain.java
+++ ConnectionMain.java
@@ -9,5 +9,12 @@ public class ConnectionMain {
         Gson gson = new Gson();
         List<Userexample> lista = gson.fromJson(txtJson, List.class);
         System.out.println("lista de registros: " + lista.size());// para ver el numero de datos que llegan
+
+        for(Userexample user:lista){
+           System.out.println(user.getUserId());
+           //imprimir solo los ID difetentes
+           //imprimir los Id que pertenecia a los Id diferente
+           
+    }
 }
 }
