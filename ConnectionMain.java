f391bbdb19d4a016b034de823d2f6197a245506f
Juan <1627908c@umich.mx>
2023-10-15 21:40:16 -0600

    La tarea

--- ConnectionMain.java
+++ ConnectionMain.java
@@ -1,16 +1,38 @@
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class ConnectionMain {
     public static void main(String[] args) {
         Conexion con = new Conexion();
         String txtJson = con.sendGet();
-        // System.out.println(txtJson);
         Gson gson = new Gson();
-        List<UserExample> lista = gson.fromJson(txtJson, List.class);
-        System.out.println("Numero de registros: " + lista.size());
-        for (UserExample user : lista) {
-            System.out.println(user.getUserId());
+        Type type = new TypeToken<List<UserExample>>() {}.getType(); 
+        List<UserExample> lista = gson.fromJson(txtJson, type);
+        List<Integer> userIds = new ArrayList<>();
+        List<List<Integer>> idLists = new ArrayList<>();
+        
+        for(UserExample user : lista){
+         int userId = user.getUserId();
+         int id = user.getId();
+         int index = userIds.indexOf(userId);
+           
+            if(index == -1){
+                userIds.add(userId);
+                idLists.add(new ArrayList<Integer>());
+                index = idLists.size() - 1;
+            }
+            idLists.get(index).add(id);
+        }
+        
+        System.out.println("userId" + userIds.toString());
+        
+        for(int i = 0; i < userIds.size(); i++){
+            int userId = userIds.get(i);
+            List<Integer> ids = idLists.get(i);
+            System.out.println("userId =" + userId + ", id" + ids.toString());
         }
     }
 }
