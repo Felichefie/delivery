a18ed043132cd4af15ca0a140356bab18dfda03a
Alexis <1701450H@umich.mx>
2023-10-20 08:58:22 -0600

    También jaló la base de datos tu

---
+++ DbConnection.java
@@ -0,0 +1,47 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DbConnection {
+    public static void main(String[] args) {
+        Connection conn ;
+        ResultSet rset ;
+        
+
+        String URL= "jdbc:mysql://clase-progra2.cii6bjvpag5z.us-east-2.rds.amazonaws.com";
+        String user= "alumno";
+        String pass = "alumnoPrueba1";
+        String query = "SELECT * FROM progra2.users";
+        String queryInsert= "INSERT INTO progra2.users (user_name,first_lastname,second_lastname,name,birthday,email)"
+        + "VALUES ('BoingDGuayaba','Birria','Tatemada','Alexis','2023-10-20','taxistafogoz69@gmail.com')";
+        
+        try {
+            conn = DriverManager.getConnection(URL, user, pass);
+            Statement statement = conn.createStatement();
+            rset = statement.executeQuery(query);
+
+            while (rset.next()) {
+                System.out.println(rset.getString(1)
+                + " " + rset.getString(2)
+                + " " + rset.getString(3)
+                + " " + rset.getString(4)
+                + " " + rset.getString(5)
+                + " " + rset.getString(6)
+                + " " + rset.getString(7) 
+                );
+               
+        } 
+        //PreparedStatement preState = conn.prepareStatement(queryInsert);
+        //preState.execute();
+        statement.execute(queryInsert);
+         conn.close();
+        }
+
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
