7630479f2bb397f5418c9178305f6c09aa3ee52c
SucioDan7u7 <143421314+SucioDan7u7@users.noreply.github.com>
2023-10-29 23:05:59 -0600

    Falta

--- ConnectionMain.java
+++ ConnectionMain.java
@@ -1,38 +1,44 @@
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 public class ConnectionMain {
     public static void main(String[] args) {
-        Conexion con = new Conexion();
+        /*Conexion con = new Conexion();
         String txtJson=con.sendGet();
-        System.out.println(txtJson);
+        System.out.println(txtJson);*/
 
-        User u = new User(1, 0, "titulo texto", "txtJson");
+        User u = new User(1, 0, "TituloTexto", "Cuerpo");
         String resJson;
         Serializar serial = new Serializar();
-        res.Json = serial.user(u);
-        System.out.println (res.Json);
-        serial.userAdd(u);
-        res.Json=serial Lista_json();
-        System.out.println (res.Json);
+        resJson = serial.serialUser(u);
+        System.out.print(resJson);
+        serial.UserAdd(u);
+        resJson = serial.lista_json();
+        System.out.println (resJson);
+
+        User u1 = new User (1, 1, "Otro Usuario", "Cuerpo2");
+        resJson = serial.serialUser(u1);
+        serial.UserAdd(u1);
+        System.out.println(resJson);
+
         
 
 
-        Gson gson = new Gson();
+        /*Gson gson = new Gson();
 
 
-        //List<Userexample> lista= gson.fromJson(txtJson, List.class);
+        List<Userexample> lista= gson.fromJson(txtJson, List.class);
         Type type =new TypeToken<List<Userexample>>() {}.getType();
         List<Userexample> lista = gson.fromJson(txtJson, type);
-        //System.out.println(lista.size());
+        System.out.println(lista.size());
         System.out.println("numero de registro:" + lista.size());
 
         for(Userexample user : lista) {
             System.out.println ("USER ID: " + user.getUserId());
-            System.out.println ("ID: " + user.getId());
+            System.out.println ("ID: " + user.getId());*/
 
         }
     }
-}
--- Dbconnection.java
+++ Dbconnection.java
@@ -4,52 +4,77 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Struct;
 import javax.lang.model.util.ElementScanner6;
 
 
 
 public class Dbconnection{
-    static Connection conn;
-public static void main (String [] args) {
-    ResultSet rset;
+
+    static Connection conn= null;
+    public static void main (String [] args) {
+    
+    //ResultSet rset;
 
     String URL = "jdbc:mysql://clase-progra2.cii6bjvpag5z.us-east-2.rds.amazonaws.com";
     String user = "alumno";
     String pass ="alumnoPrueba1";
+    String mail = "1578947d@umich.mx";
+    String numero = "4432286692";
+    int idUser = 1;
+    String calle = "Nicolas Leon";
+    int number = 109;
 
+    System.out.println("Conectando a la base...");
     
+    String query = "SELECT * FROM progra2.users";
     Dbconnection dbConn = new Dbconnection(URL, user, pass);
-    User u = dbConn.getUser("perlaPP@umich.mx");
+    UserExample u = dbConn.getUser(mail, numero);
+    Direccion a = dbConn.getAddress(u.getId(), query, number);
+
     if (u.getId()== 0){
         System.out.println("No existe");
         dbConn.insertnewUser (u);
-    }
-        else {
+    }else{
             System.out.println(u.getId());
             System.out.println(u.getUsername());
+        }
+
+    if(a.getId() == 0){
+            System.out.println("Este usurio no tiene direccion: ");
+            dbConn.insertNewDireccion(u);
+        }else{
+            System.out.println(a.getId());
+            System.out.println(a.getcalle());
+            System.out.println(a.getnumero());
         }
         try {
             conn.close();
         }catch (SQLException e){
             e.printStackTrace();
         }
+    }
 
-boolean insertnewUser (User user){
-    String queryInsert = "INSERT INTO progra2.users (user_name, first_lastname, second_lastname, birthday, email)"
-                        + "VALUES("+ user.getUsername() +",'Pantaleon','Bedolla',"+ user.getName() +",'2023-10-20','PPB@gmail.com')";
+    boolean insertnewUser (UserExample user){
+        String queryInsert = "INSERT INTO progra2.users(user_name, first_lastname, second_lastname, name, birthday, email)"
+                        + "VALUES('"+ user.getUsername() +"','Pantaleon','Bedolla',"+ user.getName() +",'2023-10-20','PPB@gmail.com'"+ user.getEmail() + "')'";
+                        PreparedStatement preState;
+
                         try {
-                            PreparedStatement preState = conn.prepareStatement(queryInsert);
+                            PreparedStatement preStatement = conn.prepareStatement(queryInsert);
                             preState.execute();
-                            return true;}
-                            catch (SQLException e) {
+                            System.out.println("Se ha creado un nuevo usuario");
+                            return true;
+                            } catch (SQLException e) {
                                 e.printStackTrace();
+                                System.out.println("No se ha podido crear un nuevo usuario");
                                 return false;
                                 }
 }
     
-    try{
+    /*try{
         conn = DriverManager.getConnection(URL, user, pass);
-        /*Statement statement =conn.createStatement();
+        Statement statement =conn.createStatement();
         rset = statement.executeQuery(query);
         while(rset.next()){
                             System.out.println(rset.getString(1)
@@ -64,31 +89,35 @@ boolean insertnewUser (User user){
         PreparedStatement preState = conn.prepareStatement(queryInsert);
         preState.execute();
         
-        conn.close();*/
+        conn.close();
         }
         
         catch (SQLException e) {
         e.printStackTrace();
         }
-}
+}*/
+
 Dbconnection(String URL, String user, String pass){
+    try{
+        conn = DriverManager.gerConnection(URL, user pass);
+    } 
+    catch (SQLException e) {
+        e.printStackTrace();
+    }
 
 }
 
-Direccion direccion(int idUser){
-
-
-}
-User getUser(String email){
-    User user=
-
-    String query = "SELECT * FROM progra2.users WHERE progra2.users.email=" + email;
-    Statement statement = conn.createStatement();
+UserExample getUser(String mail, String phone){
+    UserExample user = new UserExample();
+    String query = " SELECT * FROM progra2.users u WHERE u.email = '" + email + "' OR u.phone_number ='" + numero + "'";
+    Statement statement;
     ResultSet rset;
-    rset = statement.executeQuery(query);
     
-
-
+    
+    try{ 
+        statement = conn.createStatement();
+        rset = statement.executeQuery(query);
+    
     while (rset.next ()){
         System.out.println(rset.getString(1)
         + " " + rset.getString(2)
@@ -97,16 +126,69 @@ User getUser(String email){
         + " " + rset.getString(5)
         + " " + rset.getString(6)
         + " " + rset.getString(7)
+        + " " + rset.getString(8)
+        + " " + rset.getString(9)
         );
-        user.setId(Integer.parseInt(rset.getString(1)));
-        user.setName(rset.getString(5));
-        user.setUsername(rset.getString(2));
-    }
-    catch(SQLException e){
 
+        user.setId(Integer.parseInt(rset.getString(1)));
+        user.setUsername(rset.getString(2));
+        user.setFirts_lastname(rset.getString(3));
+        user.setSecond_lastname(rset.getString(4));
+        user.setName(rset.getString(5));
+        user.setName(rset.getString(6));
+        user.setEmail(rset.getString(7));
+        user.setGender(rset.getString(8));
+        user.setPhone_number(rset.getString(9));
+        
+    }
+}
+    catch(SQLException e){
+        System.out.print("Error en query");
+        user.setId(1);
     }
     return user;
+}
 
+Direccion getAddress (int idUser, String calle, int numero){
 
+    Direccion direc = new direc ();
+    String query = "SELECT * FROM progra2.address a WHERE a.id_User = " + idUser + "AND a.street = ' Nicolas Leon ' AND a.number = 109";
+    System.out.println(query);
+    ResultSet rset;
+    Statement statement;
+
+    try{ 
+        statement = conn.createStatement();
+        rset = statement.executeQuery(query);
+
+        while (rset.next ()){
+        System.out.println(rset.getString(1)
+        + " " + rset.getString(2)
+        + " " + rset.getString(3)
+        + " " + rset.getString(4)
+        + " " + rset.getString(5)
+        + " " + rset.getString(6)
+        + " " + rset.getString(7)
+        + " " + rset.getString(8)
+        + " " + rset.getString(9)
+        );
+
+        address.setId(Integer.parseInt(rset.getString(1)));
+        address.setIdUser(Integer.getString(2));
+        address.setStreet(rset.getString(3));
+        address.setNumber(Integer.getString(4));
+        address.setNumber_two(rset.getString(5));
+        address.setNeighborhood(rset.getString(6));
+        address.setCity(rset.getString(7));
+        address.setState(rset.getString(8));
+        address.setCountry(rset.getString(9));
+        address.setPostal_code(rset.getString(10));
+        address.setGps_lat(rset.getString(11));
+        address.setGPs_lon(rset.getString(12));
+    }
+}
+catch (Exception e){
+    System.out.println("Error en query");
+    address.setId(1);
 }
 }
