b55b01a94eaedeb49086578172b1b34707b3461d
FranciscoRamirez <1567126x@umich.mx>
2023-11-06 08:01:07 -0600

    Evaluacion1

--- evaluacion1.java
+++ evaluacion1.java
@@ -7,6 +7,21 @@ public class evaluacion1 {
     private double impuestoEspecial;
     private String tipoImpuesto;
 
+    private double[][] tabla = {
+        {0.01, 0.02, 0, 1.92},
+        {368.11, 736.22, 7.05, 6.4},
+        {3124.36, 6248.72, 183.45, 10.88},
+        {5490.76, 10981.52, 441, 16},
+        {6382.81, 12765.62, 583.65, 17.92},
+        {7641.91, 15283.82, 809.25, 21.36},
+        {15412.81, 30825.62, 2469.15, 23.52},
+        {24292.66, 48585.32, 4557.75, 30},
+        {46378.51, 92757.02, 11183.40, 32},
+        {61838.11, 123676.22, 16130.55, 34},
+        {185514.31, 371028.62, 58180.35, 35}
+    };
+
+
     // Constructor
     public evaluacion1(double ingresoMensual, String concepto, double porcentajeImpuesto, double impuestoEspecial, String tipoImpuesto) {
         this.ingresoMensual = ingresoMensual;
