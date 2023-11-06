6febc3518aadc837f7a141cf909ca55effee5d8b
Jaime Jair Raya Castellon <143474279+JAIRCASTELLON@users.noreply.github.com>
2023-11-06 08:42:22 -0600

    evaluacion 1

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
-public class main {
-    public static void main(String[] args){
-        System.out.println("probando git");
+public class Main{
+    public static void main(String[] args) {
+        println("probar git");
     }
 }
