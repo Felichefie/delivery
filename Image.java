79037ede657fef319a4929d010175d6e3c698c7e
Elena <2134664d@umich.mx>
2023-11-17 09:35:07 -0600

    ..

--- Image.java
+++ Image.java
@@ -3,18 +3,22 @@ import javax.swing.*;
 public class Image {
     private JFrame frm_image;
 
-    Image
-() { // es el constructor
+    Image() { // es el constructor
         frm_image = new JFrame();
-        
+        JLabel lblImage = new JLabel();
+        lblImage.setIcon(new ImageIcon("carrito.png"));
+        lblImage.setBounds(20,10,400,400);
 
-         frm_image.setSize(400, 500);
-         frm_image.setLayout(null);
-         frm_image.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frm_image.add(lblImage);
+        frm_image.setSize(600, 700);
+        frm_image.setLayout(null);
+        frm_image.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     } 
-
     void show() { //Metodo
         frm_image.setVisible(true);
     }
-
+public static void main(String[] args) {
+    Image im = new Image();
+    im.show();
+}
 }
