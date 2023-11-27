e2a45d8ee4bacdd330bd43dcb239329d2cfc6ffa
Maria Itzel Hernandez Marin <121146186+itzelhde@users.noreply.github.com>
2023-11-26 21:24:26 -0600

    desastreee

--- Userexample.java
+++ Userexample.java
@@ -1,35 +1,90 @@
-public class Userexample {
+public class UserExample {
+    
     private int userId;
     private int id;
     private String title;
     private String body;
+    private String name;
+    private String userName;
+    private String email;
+    private int phoneNumber;
 
-    public Userexample(){
-    }
-
-    public int getUserId(){
-        return this.userId;
-    }
-    public int getId(){
-        return this.id;
-    }
-    public String getTitle(){
-        return this.title;
-    }
-    public String getBody(){
-        return this.body;
-    }
-    public void setUserId(int userId){
-        this.userId=userId;
-    }
-    public void setId(int Id){
-        this.id=Id;
-    }
-    public void setTitle(String title){
-        this.title=title;
-    }
-    public void setBody(String body ){
-        this.body=body;
+    public UserExample(){
+        userId = 0;
+        id = 0;
+        title = "";
+        body = "";
     }
 
+    public UserExample(int userId, int id, String title, String body){
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
